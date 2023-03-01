912a6e5fe0e1ca892d7dbb7a0b103709eb6affdc
favourbadakin <favourbadakin@gmail.com>
2023-03-02 00:09:36 +0100

    improved functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ const App = () => {
   const [location, setLocation] = useState("");
   const [weatherData, setWeatherData] = useState({});
 
-  
-
   const handleChange = (event) => {
     setLocation(event.target.value);
   };
@@ -15,18 +13,18 @@ const App = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     setLocation(location);
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
-        )
-        .then((res) => {
-          setWeatherData(res.data);
-        }); 
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}`
+      )
+      .then((res) => {
+        setWeatherData(res.data);
+      });
   };
 
   return (
     <main className="px-4 py-8 lg:px-8 bg-hero-image bg-no-repeat bg-cover h-screen text-white">
-      <form className="grid place-items-center" onSubmit={handleSubmit}>
+      {/* <form className="grid place-items-center" onSubmit={handleSubmit}>
         <input
           className="border-white border-2 bg-transparent p-2 w-[80%] lg:w-[400px] rounded-lg focus:outline-none"
           placeholder="Enter a Location"
@@ -35,7 +33,60 @@ const App = () => {
           type="text"
         />
         <button>Search</button>
+      </form> */}
+
+      <form className="flex items-center" onSubmit={handleSubmit}>
+        <label htmlFor="simple-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative w-full">
+          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5 text-gray-500 dark:text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="simple-search"
+            className="bg-transparent border border-grayk-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+            placeholder="Enter a Location"
+            value={location}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          <svg
+            className="w-5 h-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+          <span className="sr-only">Search</span>
+        </button>
       </form>
+
       <div className="flex flex-col sm:gap-20 lg:gap-60">
         <div className="text-center">
           <p className="text-sm mt-16">
